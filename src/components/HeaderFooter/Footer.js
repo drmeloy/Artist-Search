7bef91ffbb344e5db82e1d1295f27e33e7b8ab6f
drmeloy <drmeloy@gmail.com>
2020-03-20 12:24:49 -0700

    working on footer image

--- src/components/HeaderFooter/Footer.js
+++ src/components/HeaderFooter/Footer.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import styles from './Footer.css';
+import pic from '../../assets/header.png';
 
 export default class Footer extends Component {
   render() {
     return (
       <>
         <footer className={styles.Footer}>
-          <img className={styles.headerImg} src="src/assets/header.png" /> 
+          <img className={styles.headerImg} src={pic} /> 
           <p>&copy; 2020 -  Nick Brimmer ||  Dr. Mellow ||  Aaron Edward Glenn</p>
           
         </footer>
